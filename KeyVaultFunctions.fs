76cf55f3c9a2071b2788e2c7397e6b44a831ecdd
Mike Treit <mtreit@ntdev.microsoft.com>
2019-12-04 16:34:40 -0800

    An F# program that hangs when attempting to authenticate to KeyVault.

---
+++ KeyVaultFunctions.fs
@@ -0,0 +1,19 @@
+﻿namespace Test
+
+module KeyVaultFunctions =
+    open Microsoft.Azure.KeyVault
+    open Microsoft.Azure.Services.AppAuthentication
+    open System.Threading.Tasks
+
+    let tokenProvider = new AzureServiceTokenProvider()
+
+    let callback a b c =
+        printfn "In callback with %A %A %A!" a b c
+        let callbackResult = tokenProvider.KeyVaultTokenCallback.Invoke(a, b, c)
+        printfn "Callback finished"
+        callbackResult
+
+    let KeyVaultTest =
+        let kvc = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(callback))
+        let secret = kvc.GetSecretAsync("https://SomeKeyVault.vault.azure.net/Secrets/SomeSecret").GetAwaiter().GetResult().Value
+        secret
---
+++ Program.fs
@@ -0,0 +1,6 @@
+﻿[<EntryPoint>]
+let main argv =
+    printfn "Trying to get an access token"
+    printfn "%s" Test.KeyVaultFunctions.KeyVaultTest
+    printfn "Finished"
+    0
